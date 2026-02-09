62b8a93ef47d9b6e2ac6c083fb7490cfdb76263d
Sharat Kubasad <152896503+SHARAT1535@users.noreply.github.com>
2026-02-09 09:45:30 +0000

    day 1 of javascript

---
+++ 2_variables.js
@@ -0,0 +1,29 @@
+
+const aadharno = "123456789012"; //constant once declared cannot be changed
+let name ="Sachin"; //initializing string 
+accountcity = "Pune";//initializing string 
+age = 45;
+
+console.log(aadharno);
+console.log(name);
+console.log(accountcity);
+console.log("  ")
+//aadharno = "5645"; //cannot change aadharno 
+name ="asc";//changing name 
+accountcity = "Pune";//changing account city 
+
+console.log(age)
+console.log(aadharno);
+console.log(name);
+console.log(accountcity);
+console.log("  ")
+
+undefined //not know what type of variable it is object type will be undefined 
+let state = null //null value object type will be null
+
+//checking types of variable 
+console.log(typeof "aadharno")
+console.log(typeof name)
+console.log(typeof age )
+console.log(typeof undefined)
+console.log(typeof null)
