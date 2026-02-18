e98ccaab7f149e89980bdde8c2201a474cf521b5
SHARAT1535 <sharat050604@gmail.com>
2026-02-18 11:09:02 +0530

    day 6 of js

---
+++ .github/14_literals.js
@@ -0,0 +1,10 @@
+const name = "JOhn Doe"
+const msg= "hello" + " " + name
+console.log(msg)
+
+const msg2 = `hello ${name}`
+console.log(msg2)
+
+const anothermsg=`hello ${name}
+welcome to mailing system `
+console.log(anothermsg)
